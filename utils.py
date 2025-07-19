c7be0f9d9d1c3af0ca30f40745dc16ebe8305df0
Robert Stanton <robertstanton95@gmail.com>
2025-07-19 17:12:04 +0100

    Restructure and functionality

--- utils.py
+++ utils.py
@@ -1,9 +1,48 @@
 import configparser
 from pymongo import MongoClient
+import os
 
-def init_db(db):
+def get_config(test=False):
+
+    root = os.path.dirname(__file__)
+    config = configparser.ConfigParser()
+    config.read_file(open(root+'/config.ini'))
+
+    return config['DEFAULT'] if not test else config['TEST']
     
-    return MongoClient(db)
+def get_url(config):
+
+    subdomain, start = config['subdomain'], config['start']
+
+    return f"https://{subdomain}.zendesk.com/api/v2/incremental/tickets?start_time={start}"
+
+def init_db(config):
+    
+    host, database , collection = config['host'], config['database'], config['collection']
+
+    database = MongoClient(host)[database]
+    
+    return database[collection]
+
+def store(collection, dictionary):
+    collection.insert_one(dictionary)
+
+
+
+test_dict = {
+    "Hello": "Dict"
+}
+
+
+
+config = get_config(test=True)
+print(get_url(config))
+print(init_db(config))
+store(init_db(config), test_dict)
+
+# tickets = init_db(db_string, db, collection)
+# tickets.insert_one(test_dict)
+
 
 
 
